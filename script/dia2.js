5d196ddb4346d5f86c76c462a9b89520f442989d
Ranzer-Dev <rodolfo.antunes9@gmail.com>
2023-04-18 20:55:08 -0300

    melhorando css adicionando alguns scripts para 'conversar' com o html e adicionando um formulário

--- script/dia2.js
+++ script/dia2.js
@@ -1,22 +1,30 @@
 
 
-let nome = document.querySelector(".aqui[value='text']");
-//let idade = prompt("Qual sua idade?");
-//let linguagem = prompt("Qual linguagem de programação você esta aprendendo?");
-//let gosta = prompt("Você gosta de estudar "+linguagem+" ? Responda com o 1 para 'Sim' e 2 para 'Não'.");
+let nome = document.querySelector(".nome");
+let idade = document.querySelector(".idade");
+let linguagem = document.querySelector(".linguagem");
+let botao = document.querySelector(".botao")
+let gosta;
 let preso;
 
-function interacao(){
 
-    alert("Olá "+ nome + ", você tem "+  idade + " anos e já está aprendendo a linguagem " + linguagem+ "!");
+botao.onclick = function() {
 
-    if(gosta == 1) {
-        alert("Muito bom! Continue estudando e você terá muito sucesso");
-        preso = 2;
-    } else if (gosta == 2) {
-        alert("Ahh que pena... Já tentou aprender outras linguagens?");
-        preso = 2;
-    } else {
-            alert("você deve responder com 1 ('Sim') ou 2('Não')");
+        alert("Olá "+ nome.value + ", você tem "+  idade.value + " anos e já está aprendendo a linguagem " + linguagem.value+ "!");
+
+        gosta = prompt("você gosta de aprender essa linguagem? (1) 'sim' (2) 'não'")
+
+        for(let i = 0; i <= 1; i++ ) {
+
+        if(gosta == 1) {
+            alert("Muito bom! Continue estudando e você terá muito sucesso");
+            preso = 2;
+        } else if (gosta == 2) {
+            alert("Ahh que pena... Já tentou aprender outra linguagen?, não desista sei que vai achar alguma que goste");
+            preso = 2;
+        } else {
+                alert("você deve responder com 1 ('Sim') ou 2('Não')");
+                i = 0;
+        }
     }
-} 
+}
