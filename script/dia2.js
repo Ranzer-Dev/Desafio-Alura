656debfcb3a03ea31bdcf8f7010a38ec74c6252c
Ranzer-Dev <rodolfo.antunes9@gmail.com>
2023-04-21 08:38:08 -0300

    código dia 2

--- script/dia2.js
+++ script/dia2.js
@@ -5,7 +5,6 @@ let idade = document.querySelector(".idade");
 let linguagem = document.querySelector(".linguagem");
 let botao = document.querySelector(".botao")
 let gosta;
-let preso;
 
 
 botao.onclick = function() {
@@ -18,10 +17,8 @@ botao.onclick = function() {
 
         if(gosta == 1) {
             alert("Muito bom! Continue estudando e você terá muito sucesso");
-            preso = 2;
         } else if (gosta == 2) {
             alert("Ahh que pena... Já tentou aprender outra linguagen?, não desista sei que vai achar alguma que goste");
-            preso = 2;
         } else {
                 alert("você deve responder com 1 ('Sim') ou 2('Não')");
                 i = 0;
