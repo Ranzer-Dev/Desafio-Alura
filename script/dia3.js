ccb4c33bc903f093a8bd3d2d359745a678b46880
Ranzer-Dev <rodolfo.antunes9@gmail.com>
2023-04-24 06:50:03 -0300

    codigo do dia 3 ok

--- script/dia3.js
+++ script/dia3.js
@@ -3,34 +3,14 @@ let ok = document.querySelector(".ok");
 let respostas = [];
 let perguntas = [["parabens você escolheu Front-End, dentro dessa especialização qual desses frameworks você vai se especializar? React = 1, Vue = 2", "parabens você escolheu Back-End, dentro dessa especialização qual desses frameworks você quer se especializar? C# = 1, Java = 2"], ["React, Muito Bom!!, vai querer ser fullstack = 1, ou prefere ficar fera na área escolhidas = 2 ?", "Vue, Muito Bom!!, vai querer ser fullstack = 1, ou prefere ficar fera na área escolhidas = 2"], ["C#, Muito Bom!!, vai querer ser fullstack = 1, ou prefere ficar fera na área escolhidas = 2", "Java, Muito Bom!!, vai querer ser fullstack = 1, ou prefere ficar fera na área escolhidas = 2"], ["tem mais alguma tecnologia que você gostaria de aprender? 1 = ok , 2 = agora não", "Quais seriam? (aperte 2 para sair)"]]
 let campoResposta = document.querySelector(".resposta");
-let objetivos = [];
-let objetivo;
 let entraLoop = false;
 let n = 0;
-
-ok.onclick = function () {
-    armazenaResposta(n);
-    acrescentaResposta();
-    desencadeiaperguntas();
-}
-
-campoResposta.onkeydown = function (e) {
-    if (e.key == "Enter" && campoResposta === document.activeElement) {
-        armazenaResposta(n);
-        acrescentaResposta();
-        desencadeiaperguntas();
-    }
-};
-
-window.onload = function () {
-    campoResposta.focus();
-}
 
 function acrescentaResposta() {
     if ((campoResposta.value == 1 || campoResposta.value == 2) && entraLoop == false) {
         n = n + 1;
         campoResposta.value = null;
-    } else if (n < 3){
+    } else if (n < 3) {
         alert("Por favor, responda com 1 ou 2");
     }
 }
@@ -39,7 +19,7 @@ function armazenaResposta(n) {
     respostas[n] = document.querySelector(".resposta").value;
 }
 
-function desencadeiaperguntas() {
+function desencadeiaPerguntas() {
 
     if (respostas[0] == 1 && entraLoop == false) {
         pergunta.innerHTML = perguntas[0][0];
@@ -70,18 +50,45 @@ function desencadeiaperguntas() {
     }
 }
 
- function tecnologias(){
-    for (let i = 0; i < 1; i++) { 
-        if(respostas[3] == 1 || (respostas[3] != null && respostas[3] != 2)){
-            objetivos.push(campoResposta.value);
+function tecnologias() {
+    for (let i = 0; i < 1; i++) {
+        if (respostas[3] == 1 || (respostas[3] != null && respostas[3] != 2)) {
             pergunta.innerHTML = perguntas[3][1];
+            if(respostas[3] != 1){
+            adicionaElemento();
+            }
             campoResposta.value = null;
         } else if (respostas[3] == 2) {
-            alert("até agora você quer aprender: " + "\n" + objetivos + "\n" + "novas linguagems");
             campoResposta.hidden = true;
             ok.hidden = true;
             pergunta.hidden = true;
             i = 1;
         }
     }
+}
+
+function adicionaElemento() {
+    let teste = document.createElement("input");
+    let divLinguagens = document.querySelector(".divLinguagens");
+    divLinguagens.insertBefore(teste, null);
+    teste.readOnly = true;
+    teste.value = campoResposta.value; 
+}
+
+ok.onclick = function () {
+    armazenaResposta(n);
+    acrescentaResposta();
+    desencadeiaPerguntas();
+}
+
+campoResposta.onkeydown = function (e) {
+    if (e.key == "Enter" && campoResposta === document.activeElement) {
+        armazenaResposta(n);
+        acrescentaResposta();
+        desencadeiaPerguntas();
+    }
+};
+
+window.onload = function () {
+    campoResposta.focus();
 }
