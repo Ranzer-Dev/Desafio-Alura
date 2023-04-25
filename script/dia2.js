8b2f03a8e3c8fce0ca19db4066726bdd673e8b0c
Ranzer-Dev <rodolfo.antunes9@gmail.com>
2023-04-25 19:47:37 -0300

    melhorando o codigo e mudando visual dia4

--- script/dia2.js
+++ script/dia2.js
@@ -1,27 +1,46 @@
-
-
 let nome = document.querySelector(".nome");
 let idade = document.querySelector(".idade");
 let linguagem = document.querySelector(".linguagem");
 let botao = document.querySelector(".botao")
 let gosta;
 
+function armazenaDados() {
+    alert("Olá " + nome.value + ", você tem " + idade.value + " anos e já está aprendendo a linguagem " + linguagem.value + "!");
 
-botao.onclick = function() {
+    gosta = prompt("você gosta de aprender essa linguagem? (1) 'sim' (2) 'não'")
 
-        alert("Olá "+ nome.value + ", você tem "+  idade.value + " anos e já está aprendendo a linguagem " + linguagem.value+ "!");
+    for (let i = 0; i < 2; i++) {
 
-        gosta = prompt("você gosta de aprender essa linguagem? (1) 'sim' (2) 'não'")
-
-        for(let i = 0; i <= 1; i++ ) {
-
-        if(gosta == 1) {
+        if (gosta == 1) {
             alert("Muito bom! Continue estudando e você terá muito sucesso");
         } else if (gosta == 2) {
             alert("Ahh que pena... Já tentou aprender outra linguagen?, não desista sei que vai achar alguma que goste");
-        } else {
-                alert("você deve responder com 1 ('Sim') ou 2('Não')");
-                i = 0;
+        } else if (gosta != 1 || gosta != 2) {
+            alert("você deve responder com 1 ('Sim') ou 2('Não')");
         }
+    }
+}
+
+nome.onkeydown = function (e) {
+    if (e.key == 'Enter' && nome.value != '' && idade.value != '' && linguagem.value != '') {
+        armazenaDados()
+    }
+}
+
+idade.onkeydown = function (e) {
+    if (e.key == 'Enter' && nome.value != '' && idade.value != '' && linguagem.value != '') {
+        armazenaDados()
+    }
+}
+
+linguagem.onkeydown = function (e) {
+    if (e.key == 'Enter' && nome.value != '' && idade.value != '' && linguagem.value != '') {
+        armazenaDados()
+    }
+}
+
+botao.onclick = function () {
+    if (e.key == 'Enter' && nome.value != '' && idade.value != '' && linguagem.value != '') {
+        armazenaDados()
     }
 }
--- script/dia4.js
+++ script/dia4.js
@@ -1,11 +1,18 @@
+/*
 const numero = Math.floor(Math.random()*10);
-let adivinhacao = prompt("Qual numero estou pensando? Dica é de 1 a 10");
-let chances = 3;
+let adivinhacao = prompt("Qual numero estou pensando? Dica é de 1 a 10, você tem "+chances);
+var chances = 2;
+*/
+
+let criaBotões = document.createElement('button');
+let adicionaBotões = document.body.insertBefore(criaBotões, null);
+let 
 
 
-for(let i = 3; i > 0 ; i--){
-    if(adivinhacao != null){
-        if(numero === adivinhacao){
+/*
+for(let i = 2; i > 0 ; i--){
+    if(adivinhacao != ''){
+        if(adivinhacao == numero){1
             alert("Parabens, esse era o numero que eu estava pensando")
             i = 0;
         } else {
@@ -15,13 +22,14 @@ for(let i = 3; i > 0 ; i--){
     } else {
         alert("preemcha o campo");
         adivinhacao = prompt("que pena, você errou, mas você tem "+chances+" Chances boa sorte");
-        i = 3;
+        i = 2;
     }      
 }
 
 
-if (chances === 0 && adivinhacao !== numero){
+if (chances === 0 && adivinhacao != numero){
     alert("você perdeu o jogo :(, o numero certo era "+numero);
-} else {
+} else if (adivinhacao == numero) {
     alert("Você acertou!!!")
 }
+*/
