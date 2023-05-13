3cba83e8f1a534d625282f5544683be609dceea6
Ranzer-Dev <rodolfo.antunes9@gmail.com>
2023-05-13 20:16:04 -0300

    mudando o codigo do dia 7 para 'conversar' melhor com o css e html

--- script/dia7.js
+++ script/dia7.js
@@ -1,52 +1,60 @@
-function soma(a, b) {
-    let resultado = a + b;
-    alert(resultado);
-}
+let botoes = document.querySelectorAll("button");
+let visor0 = document.querySelector(".visor0");
+let visor1 = document.querySelector(".visor1");
+let operacao = document.querySelector(".operacao");
+let numeros = [];
 
-function subtaracao(a, b) {
-    let resultado = a - b;
-}
-
-function multiplicacao(a, b) {
-    let resultado = a * b;
-}
-
-function divisao(a, b) {
-    let resultado = a / b;
-}
-
-function sair() {
-    alert("Até a proxima");
-}
-
-let continuar = true;
-
-for (let i = 0; continuar; i++) {
-
-    funcao = prompt("qual operação deseja fazer? soma (+), menos(-),vezes(*) ou divisão(/)"+"\n"+"sair da cauculadora(s)?")
-
-    if (continuar == true && funcao == "+" || funcao == "-" || funcao == "*" || funcao == "/") {
-        numero1 = Number(prompt("qual seria o primeiro numero?"));
-        numero2 = Number(prompt("qual seria o segundo numero?"));
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+        if(botoes[i].innerHTML != "+" &&
+           botoes[i].innerHTML != "-" &&
+           botoes[i].innerHTML != "/" &&
+           botoes[i].innerHTML != "*" &&
+           botoes[i].innerHTML != "="){
+        visor1.value = visor1.value + botoes[i].innerHTML;
+        numeros[0] = Number(visor1.value);
     }
-
-    switch (funcao) {
-        case "+":
-            soma(numero1, numero2);
-            break;
-        case "-":
-            subtaracao(numero1, numero2);
-            break;
-        case "*":
-            multiplicacao(numero1, numero2);
-            break;
-        case "/":
-            divisao(numero1, numero2);
-            break;
-        case "s":
-            continuar = false;
-            break;
-        default:
-            alert("esclolha uma das operações");
+        if(botoes[i].innerHTML == "+"){
+            soma();
+        }
+        if(botoes[i].innerHTML == "-"){
+            subtracao();
+        }
+        if(botoes[i].innerHTML == "/"){
+            divisao();
+        }
+        if(botoes[i].innerHTML == "*"){
+            multiplicacao();
+        }
+        if(botoes[i].innerHTML == "=" ){
+            resultado();
+        }
     }
+}
+
+function soma(){
+    visor0.value = numeros[0];
+    visor1.value = null;
+    operacao.value = "+";
+}
+function subtracao(){
+    visor0.value = numeros[0];
+    visor1.value = null;
+    operacao.value = "-"
+}
+function divisao(){
+    visor0.value = numeros[0];
+    visor1.value = null;
+    operacao.value = "/"
+}
+function multiplicacao(){
+    visor0.value = numeros[0];
+    visor1.value = null;
+    operacao.value = "*"
+}
+
+function resultado(){
+    visor1.value = numeros[1] + numeros[0];
+    visor0.value = null;
+    operacao.value = null;
 }
