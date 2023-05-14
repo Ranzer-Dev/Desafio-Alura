96daba3e754e353807dfde4e9f9def8f60ad0c7f
Ranzer-Dev <rodolfo.antunes9@gmail.com>
2023-05-14 09:29:11 -0300

    terminando a logica da calculadora

--- script/dia7.js
+++ script/dia7.js
@@ -2,6 +2,7 @@ let botoes = document.querySelectorAll("button");
 let visor0 = document.querySelector(".visor0");
 let visor1 = document.querySelector(".visor1");
 let operacao = document.querySelector(".operacao");
+let resultado;
 let numeros = [];
 
 for (let i = 0; i < botoes.length; i++) {
@@ -11,8 +12,18 @@ for (let i = 0; i < botoes.length; i++) {
            botoes[i].innerHTML != "/" &&
            botoes[i].innerHTML != "*" &&
            botoes[i].innerHTML != "="){
-        visor1.value = visor1.value + botoes[i].innerHTML;
-        numeros[0] = Number(visor1.value);
+            if(visor1.value != resultado){
+                visor1.value = visor1.value + botoes[i].innerHTML;
+            } else {
+                visor1.value = null; 
+                visor1.value = visor1.value + botoes[i].innerHTML;
+            }
+        if(visor0.value == ""){
+            numeros[0] = Number(visor1.value);
+        } else {
+            numeros[1] = Number(visor1.value);
+        }
+        
     }
         if(botoes[i].innerHTML == "+"){
             soma();
@@ -27,7 +38,7 @@ for (let i = 0; i < botoes.length; i++) {
             multiplicacao();
         }
         if(botoes[i].innerHTML == "=" ){
-            resultado();
+            resultadoOperacao();
         }
     }
 }
@@ -53,8 +64,20 @@ function multiplicacao(){
     operacao.value = "*"
 }
 
-function resultado(){
-    visor1.value = numeros[1] + numeros[0];
+function resultadoOperacao(){
+    if(operacao.value == "+"){
+        resultado = numeros[0] + numeros[1];
+    }
+    if(operacao.value == "-"){
+        resultado = numeros[0] - numeros[1];
+    }
+    if(operacao.value == "*"){
+        resultado = numeros[0] * numeros[1];
+    }
+    if(operacao.value == "/"){
+        resultado = numeros[0] / numeros[1];
+    }
+    visor1.value = resultado
+    numeros[0] = resultado;
     visor0.value = null;
-    operacao.value = null;
 }
